0b2bd682a828827609f88e7db26848ef2f803126
David Harrison <43589032+david101967@users.noreply.github.com>
2023-09-25 12:08:26 -0400

    cardcontainer.py

---
+++ cardcontainer.py
@@ -0,0 +1,34 @@
+
+class Card_container:
+    def __init__(self):
+        pass
+#Randomly generate 5 cards
+    def set_innit_num_of_cards(self):
+        pass
+
+#remove first card from list and return it
+    def remove_first_card(self):
+        pass
+
+#return refrence to first card in cards list
+    def get_first_card(self):
+        pass
+
+# c is the type of card
+    def add_new_card(self,c):
+        pass
+
+#creates an internal representation of a deck of cards, either owned by player 1, player 2, or on the
+#table, based on a list of cards given by the user view/format.
+
+#lofc is of list of cards in the user format
+    def add_new_cards(self,lofc):
+        pass
+
+# Return the list of cards in the internal format
+    def get_cards(self):
+        pass
+
+#Return the list of cards in the User view format(suit,1-13)
+    def get_cards_from_user_view(self):
+        pass
